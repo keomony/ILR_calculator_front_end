3dbbc76ce1280ddf48dbb3c94fe9e8e726f71b8b
Keomony <kkhun@thoughtworks.com>
2019-10-02 15:20:28 +0100

    (Keomony/Gabriel) (#9) 😀 react-dates component working without start and end dates

--- src/components/calendarRow.js
+++ src/components/calendarRow.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'react-dates/initialize';
-import Calendar from 'react-calendar';
+import 'react-dates/lib/css/_datepicker.css';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
 import styled from '@emotion/styled';
+import * as moment from 'moment';
 
 const Container = styled.div({
   display: 'flex',
@@ -15,10 +17,24 @@ const Button = styled.button({
 });
 
 export const CalendarRow = () => {
-  
+  const initStartDate = moment([2019, 2, 11]);
+  const initEndDate = moment([2019, 3, 11]);
+  const [state, setState] = useState({ initStartDate, initEndDate });
+
   return (
     <Container>
-      <Calendar />
+      <DateRangePicker
+        startDate={null} // momentPropTypes.momentObj or null,
+        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+        endDate={null} // momentPropTypes.momentObj or null,
+        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+        onDatesChange={({ startDate, endDate }) => {
+          console.log(startDate);
+          setState({ startDate, endDate })
+        }} // PropTypes.func.isRequired,
+        focusedInput={'startDate'} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+        onFocusChange={focusedInput => () => {}} // PropTypes.func.isRequired,
+      />
       <Button>delete</Button>
     </Container>
   );
