b6ed3652287eab135906f4b6e280b88d3d63df4e
Keomony <kkhun@thoughtworks.com>
2019-10-30 14:06:22 +0000

    [Keomony/Kadir] Add and remove calendars

--- src/App.js
+++ src/App.js
@@ -5,16 +5,35 @@ import { start, end } from './lib/datesStore';
 
 const App = () => {
 
-  const [remainingDays, setRemainingDays] = useState();
-  
-  return (
-    <div>
-      <h3 align="center"> Tier 2 visa holder: Indefinite Leave to Remain </h3>
-      <CalendarRow updateStartDate={start} updateEndDate={end} id={0} setRemainingDays={setRemainingDays} />
-      <CalendarRow updateStartDate={start} updateEndDate={end} id={1} setRemainingDays={setRemainingDays} />
-      <div>The remaining days: {remainingDays}</div>
+    const [remainingDays, setRemainingDays] = useState();
+    const [calendars, setCalender] = useState([]);
+
+
+    const handleAddCalendar = () => {
+        calendars.push(0);
+        setCalender([...calendars]);
+        console.log('handleAddCalendar', calendars);
+    }
+
+    const removeCalendar = (calendarIndex) => {
+        calendars.splice(calendarIndex, 1);
+        setCalender([...calendars]);
+    }
+
+    return (
+        <div>
+        <h3 align="center"> Tier 2 visa holder: Indefinite Leave to Remain </h3>
+
+    {calendars.map((c, index) => {
+        return (<div key={index}>
+        <CalendarRow updateStartDate={start} updateEndDate={end} id={index} setRemainingDays={setRemainingDays} />
+    <button onClick={()=>{removeCalendar(index)}}>remove</button>
+    </div>)
+    })}
+<div>The remaining days: {remainingDays}</div>
+    <div><button onClick={handleAddCalendar}>add calendar</button> {calendars}</div>
     </div>
-  );
+);
 }
 
 export default App;
